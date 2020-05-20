d5a064ae9839a22a797d4385c10a76857cf7219d
Daniel Gorokhov <55414689+monidan@users.noreply.github.com>
2020-05-20 11:51:49 +0300

    Added Moudle to the local branch

---
+++ HorokhovModule/cmake-build-debug/CMakeFiles/HorokhovModule.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HorokhovModule.dir/main.cpp.o"
+  "HorokhovModule.exe"
+  "HorokhovModule.pdb"
+  "libHorokhovModule.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HorokhovModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HorokhovModule/cmake-build-debug/CMakeFiles/HorokhovModule.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HorokhovModule.
+# This may be replaced when dependencies are built.
